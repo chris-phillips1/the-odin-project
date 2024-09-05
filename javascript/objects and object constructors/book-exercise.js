b2069543049d04d063d849986d8089e0a0d1a545
Chris Phillips <cpphillips13@gmail.com>
2024-09-04 23:41:08 -0400

    Add form for adding books

--- javascript/objects and object constructors/book-exercise.js
+++ javascript/objects and object constructors/book-exercise.js
@@ -1,5 +1,11 @@
 let myLibrary = [];
 const tableElement = document.querySelector('table');
+const createBookButton = document.getElementById('createBookButton');
+
+const createBookDialog = document.getElementById('createBookDialog');
+const createBookForm = createBookDialog.querySelector('form');
+const createBookFormButtons = createBookForm.querySelector('#formButtons');
+const confirmButton = createBookDialog.querySelector("#confirmButton");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -8,7 +14,7 @@ function Book(title, author, pages, read) {
     this.read = read;
     this.info = function () {
         let bookInfo = title + ' by ' + author + ', ' + pages + ' pages, ';
-        bookInfo = read ? bookInfo += 'read' : bookInfo += 'not read yet';
+        bookInfo += read ? 'read' : 'not read yet';
         return bookInfo;
     }
 }
@@ -19,6 +25,10 @@ function addBookToLibrary(book) {
 }
 
 function displayBooks() {
+    while (tableElement.firstChild) {
+        tableElement.removeChild(tableElement.firstChild);
+    }
+
     myLibrary.forEach((book) => {
         const bookRow = document.createElement('tr');
 
@@ -29,7 +39,7 @@ function displayBooks() {
         const bookPages = document.createElement('td');
         bookPages.innerText = book.pages;
         const bookRead = document.createElement('td');
-        bookRead.innerText = book.read;
+        bookRead.innerText = book.read ? 'Yes' : 'No';
 
         bookRow.appendChild(bookTitle);
         bookRow.appendChild(bookAuthor);
@@ -39,5 +49,52 @@ function displayBooks() {
     });
 }
 
-addBookToLibrary(new Book('The Hobbit', 'J.R.R. Tolkien', 295, false));
-displayBooks();
+function createFormField(name, type, id) {
+    const formItem = document.createElement('p');
+    const formLabel = document.createElement('label');
+    const formField = document.createElement('input');
+
+    formLabel.innerText = name;
+    formField.type = type;
+    formField.id = id;
+
+    formLabel.appendChild(formField);
+    formItem.appendChild(formLabel);
+    createBookForm.insertBefore(formItem, createBookFormButtons);
+}
+
+function buildBookForm() {
+    createFormField('Title:', 'text', 'titleTextField');
+    createFormField('Author:', 'text', 'authorTextField');
+    createFormField('Pages:', 'number', 'pagesNumberField');
+    createFormField('Read:', 'checkbox', 'readCheckboxField');
+}
+
+createBookButton.addEventListener('click', () => {
+    createBookDialog.showModal();
+});
+
+cancelButton.addEventListener('click', () => {
+    createBookDialog.close();
+})
+
+confirmButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    createBookDialog.close();
+
+    let title = document.getElementById('titleTextField');
+    let author = document.getElementById('authorTextField');
+    let pages = document.getElementById('pagesNumberField');
+    let read = document.getElementById('readCheckboxField');
+
+    addBookToLibrary(new Book(title.value, author.value, pages.value, read.checked));
+
+    title.value = null;
+    author.value = null;
+    pages.value = null;
+    read.checked = false;
+
+    displayBooks();
+});
+
+buildBookForm();
