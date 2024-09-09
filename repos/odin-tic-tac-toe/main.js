db6970fb11a5a9ab6fd255c84f7f659e866bdfbe
Chris Phillips <cpphillips13@gmail.com>
2024-09-09 18:03:51 -0400

    Changes to board generation, printing, and modification

--- repos/odin-tic-tac-toe/main.js
+++ repos/odin-tic-tac-toe/main.js
@@ -1,30 +1,42 @@
+const gameboard = function Gameboard() {
+    const board = [];
+    const generateBoard = () => {
+        board.push(['  ', '  ', '  ']);
+        board.push(['  ', '  ', '  ']);
+        board.push(['  ', '  ', '  ']);
+    };
+
+    const getBoard = () => board;
+    const printBoard = () => {
+        let boardString = 'Board\n';
+        board.forEach((boardRow, index) => {
+            boardString += boardRow.join(' || ') + '\n';
+            if (index < boardRow.length - 1) {
+                boardString += '= = = = =\n';
+            }
+        });
+        console.log(boardString);
+    };
+
+    const addMoveToBoard = (playerToken, coordinates) => {
+        board[coordinates[0] - 1][coordinates[1] - 1] = playerToken;
+    };
+
+
+    return { generateBoard, getBoard, printBoard, addMoveToBoard };
+}();
+
 function Player() {
 
 }
 
-function GameRunner() {
-    const gameboard = function Gameboard() {
-        const board = [];
-        const generateBoard = () => {
-            board.push([null, null, null]);
-            board.push([null, null, null]);
-            board.push([null, null, null]);
-        };
-
-        const getBoard = () => board;
-        const printBoard = () => {
-        };
-
-        const addMoveToBoard = (player, row, column) => {
-        };
-
-
-        return { generateBoard, getBoard, printBoard, addMoveToBoard };
-    }();
-
-    gameboard.generateBoard();
-    console.log(gameboard.getBoard());
-
+function GameRunner(board) {
+    board.generateBoard();
+    board.printBoard();
+    board.addMoveToBoard('X', [2, 1]);
+    board.addMoveToBoard('0', [2, 2]);
+    board.addMoveToBoard('X', [1, 1]);
+    board.printBoard();
 }
 
-GameRunner();
+GameRunner(gameboard);
