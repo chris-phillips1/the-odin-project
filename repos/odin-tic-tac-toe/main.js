d2ae45b43ec14b3d1e8cdaa213715d4adec30fa2
Chris Phillips <cpphillips13@gmail.com>
2024-09-10 23:14:18 -0400

    Create Player constructor

--- repos/odin-tic-tac-toe/main.js
+++ repos/odin-tic-tac-toe/main.js
@@ -19,23 +19,35 @@ const gameboard = function Gameboard() {
     };
 
     const addMoveToBoard = (playerToken, coordinates) => {
-        board[coordinates[0] - 1][coordinates[1] - 1] = playerToken;
+        board[coordinates.row - 1][coordinates.column - 1] = playerToken;
     };
 
 
     return { generateBoard, getBoard, printBoard, addMoveToBoard };
 }();
 
-function Player() {
+function Player(initialToken) {
+    let token = initialToken;
+    let spaces = [];
 
+    const getToken = () => token;
+    const setToken = (newToken) => {
+        token = newToken;
+    };
+
+    const addMoveToSpacesList = (moveCoordinates) => {
+        spaces.push(moveCoordinates);
+    };
+
+    return { getToken, setToken, addMoveToSpacesList };
 }
 
 function GameRunner(board) {
     board.generateBoard();
     board.printBoard();
-    board.addMoveToBoard('X', [2, 1]);
-    board.addMoveToBoard('0', [2, 2]);
-    board.addMoveToBoard('X', [1, 1]);
+    board.addMoveToBoard('X', { row: 2, column: 1 });
+    board.addMoveToBoard('0', { row: 2, column: 2 });
+    board.addMoveToBoard('X', { row: 1, column: 1 });
     board.printBoard();
 }
 
