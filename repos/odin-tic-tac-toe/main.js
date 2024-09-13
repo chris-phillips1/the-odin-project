4c8fbeaf00d01031b68603bc141241102e83012b
Chris Phillips <cpphillips13@gmail.com>
2024-09-12 20:32:35 -0400

    Most of terminal tic tac toe

--- repos/odin-tic-tac-toe/main.js
+++ repos/odin-tic-tac-toe/main.js
@@ -19,36 +19,59 @@ const gameboard = function Gameboard() {
     };
 
     const addMoveToBoard = (playerToken, coordinates) => {
-        board[coordinates.row - 1][coordinates.column - 1] = playerToken;
+        let boardSpace = board[coordinates.row - 1][coordinates.column - 1];
+        if (boardSpace.trim().length === 0) {
+            board[coordinates.row - 1][coordinates.column - 1] = playerToken;
+            checkWin(coordinates);
+        }
+    };
+
+    const checkWin = (coordinates) => {
+        // return { hasWinner: (board[1][1] === 'X') };
     };
 
 
-    return { generateBoard, getBoard, printBoard, addMoveToBoard };
+    return { generateBoard, getBoard, printBoard, addMoveToBoard, checkWin };
 }();
 
-function Player(initialToken) {
-    let token = initialToken;
-    let spaces = [];
+function Player(playerToken, isActive) {
+    let token = playerToken;
+    let active = isActive;
 
     const getToken = () => token;
     const setToken = (newToken) => {
         token = newToken;
     };
 
-    const addMoveToSpacesList = (moveCoordinates) => {
-        spaces.push(moveCoordinates);
-    };
+    const getActive = () => active;
+    const toggleActive = () => {
+        active = !active;
+    }
 
-    return { getToken, setToken, addMoveToSpacesList };
+    return { getToken, setToken, getActive, toggleActive };
 }
 
 function GameRunner(board) {
     board.generateBoard();
-    board.printBoard();
-    board.addMoveToBoard('X', { row: 2, column: 1 });
-    board.addMoveToBoard('0', { row: 2, column: 2 });
-    board.addMoveToBoard('X', { row: 1, column: 1 });
-    board.printBoard();
+    const player1 = Player('X', true);
+    const player2 = Player('O', false);
+
+    let boardStatus = board.checkWin();
+
+    while (!(boardStatus.hasWinner)) {
+        let playerSpot = prompt('Which spot (comma-delimited): ');
+
+        if (playerSpot) {
+            playerSpot = playerSpot.split(',', 2);
+        }
+
+        const activePlayer = player1.getActive() ? player1 : player2;
+        board.addMoveToBoard(activePlayer.getToken(), { row: playerSpot[0], column: playerSpot[1] });
+        player1.toggleActive();
+        player2.toggleActive();
+        boardStatus = board.checkWin();
+        board.printBoard();
+    }
 }
 
 GameRunner(gameboard);
